bf118ba80ef10460f34636b0030b87e7cc4969ff
Daniel Graça <danielgrascas@gmail.com>
2024-03-26 16:47:05 +0000

    Rules and validation for X number of tickets (dynamic form)
    Signed-off-by: Daniel Graça <danielgrascas@gmail.com>

--- app/Livewire/Forms/TicketForm.php
+++ app/Livewire/Forms/TicketForm.php
@@ -19,6 +19,19 @@ class TicketForm extends Form
     public $price;
     public $currency;
 
+    public function rules()
+    {
+        return [
+            'tickets.*.name' => 'required|string|max:255',
+            'tickets.*.description' => 'nullable|string|max:65535',
+            'tickets.*.max_check_in' => 'required|integer',
+            'tickets.*.max_tickets_per_order' => 'required|integer',
+            'tickets.*.price' => 'required|numeric',
+            'tickets.*.currency' => 'required|string|max:3',
+            'tickets.*.sessions' => 'required|array|min:1',
+        ];
+    }
+
     public function addTicket($ticket = null)
     {
         // Add the new ticket to the $tickets array
@@ -49,6 +62,10 @@ class TicketForm extends Form
     }
 
     public function create($t, $event_id, $tickets) {
+        // validate ticket fields
+        $this->validate();
+
+        // create ticket
         $ticket = new Ticket($t);
         $ticket->event_id = $event_id;
         $ticket->save();
@@ -66,6 +83,10 @@ class TicketForm extends Form
                 continue;
             }
 
+            // validate ticket fields
+            $this->validate();
+
+            // update ticket
             $ticket = Ticket::find($t['id']);
             $ticket->fill($t);
             $ticket->save();
--- resources/views/tickets/fields-livewire.blade.php
+++ resources/views/tickets/fields-livewire.blade.php
@@ -35,11 +35,11 @@
     <x-base.form-label :tw-merge="false" for="name">{{ \App\Models\Ticket::getAttributeLabelStatic('name') }}</x-base.form-label>
     <x-base.form-input
         :tw-merge="false"
-        class="w-full {{ ($errors->has('name') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.name') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.name"
         type="text"
     />
-    @error('name')
+    @error('ticketForm.tickets.' . $index . '.name')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -49,11 +49,11 @@
     <x-base.form-label :tw-merge="false" for="description">{{ \App\Models\Ticket::getAttributeLabelStatic('description') }}</x-base.form-label>
     <x-base.form-textarea
         :tw-merge="false"
-        class="w-full {{ ($errors->has('description') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.description') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.description"
         rows="5"
     ></x-base.form-textarea>
-    @error('description')
+    @error('ticketForm.tickets.' . $index . '.description')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -63,12 +63,12 @@
     <x-base.form-label :tw-merge="false" for="max_check_in">{{ \App\Models\Ticket::getAttributeLabelStatic('max_check_in') }}</x-base.form-label>
     <x-base.form-input
         :tw-merge="false"
-        class="w-full {{ ($errors->has('max_check_in') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.max_check_in') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.max_check_in"
         type="number"
         step="1"
     />
-    @error('max_check_in')
+    @error('ticketForm.tickets.' . $index . '.max_check_in')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -78,12 +78,12 @@
     <x-base.form-label :tw-merge="false" for="max_tickets_per_order">{{ \App\Models\Ticket::getAttributeLabelStatic('max_tickets_per_order') }}</x-base.form-label>
     <x-base.form-input
         :tw-merge="false"
-        class="w-full {{ ($errors->has('max_tickets_per_order') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.max_tickets_per_order') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.max_tickets_per_order"
         type="number"
         step="1"
     />
-    @error('max_tickets_per_order')
+    @error('ticketForm.tickets.' . $index . '.max_tickets_per_order')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -93,13 +93,13 @@
     <x-base.form-label :tw-merge="false" for="price">{{ \App\Models\Ticket::getAttributeLabelStatic('price') }}</x-base.form-label>
     <x-base.form-input
         :tw-merge="false"
-        class="w-full {{ ($errors->has('price') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.price') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.price"
         type="number"
         step="1"
         step="1"
     />
-    @error('price')
+    @error('ticketForm.tickets.' . $index . '.price')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -109,11 +109,11 @@
     <x-base.form-label :tw-merge="false" for="currency">{{ \App\Models\Ticket::getAttributeLabelStatic('currency') }}</x-base.form-label>
     <x-base.form-input
         :tw-merge="false"
-        class="w-full {{ ($errors->has('currency') ? 'border-danger' : '') }}"
+        class="w-full {{ ($errors->has('ticketForm.tickets.' . $index . '.currency') ? 'border-danger' : '') }}"
         wire:model.live="ticketForm.tickets.{{ $index }}.currency"
         type="text"
     />
-    @error('currency')
+    @error('ticketForm.tickets.' . $index . '.currency')
         <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
@@ -121,11 +121,10 @@
 <div class="mb-3">
     <x-base.form-label :tw-merge="false" for="sessions">{{ \App\Models\Ticket::getAttributeLabelStatic('sessions') }}</x-base.form-label>
     @foreach($sessions as $sessionIndex => $session)
-
-        {{-- class="{{ ($errors->has('pre-approval') ? 'border-danger' : '') }}" --}}
         <x-base.form-check :tw-merge="false">
             <x-base.form-check.input
                 :tw-merge="false"
+                 class="{{ ($errors->has('ticketForm.tickets.' . $index . '.sessions') ? 'border-danger' : '') }}"
                 wire:model="ticketForm.tickets.{{ $index }}.sessions.{{ $sessionIndex }}"
                 value="ticketForm.tickets.{{ $index }}.sessions.{{ $sessionIndex }}"
                 type="checkbox"
@@ -134,7 +133,7 @@
         </x-base.form-check>
     @endforeach
 
-    @error('sessions')
+    @error('ticketForm.tickets.' . $index . '.sessions')
     <div class="mt-2 text-danger">{{ $message }}</div>
     @enderror
 </div>
